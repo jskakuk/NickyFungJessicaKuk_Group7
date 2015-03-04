efebe3c45579143c22269379d80959eaaa6792d3
jskakuk <kuk.jska@gmail.com>
2015-03-03 22:54:16 -0800

    1. added jquery for image gallery

---
+++ js/ThoughtsOn.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+  var image=new Array();
+  var current=0;
+
+  image[0]=new Image();
+  image[0].src="images/detailed_product/productdetails_mason.jpg";
+  image[1]=new Image();
+  image[1].src="images/detailed_product/productdetails_mason_closeup.jpg";
+  image[2]=new Image();
+  image[2].src="images/detailed_product/productdetails_mason_lid.jpg";
+  image[3]=new Image();
+  image[3].src="images/detailed_product/productdetails_mason_context.jpg";
+  
+
+   $("#left").click(function(){
+    if(current-1<0){
+      current=image.length-1;
+      $("#mason-image").attr("src", image[current].src);
+    }
+    else{
+      --current;
+      $("#mason-image").attr("src", image[current].src);
+    }
+  });
+
+  $("#right").click(function(){
+    if(current+1>image.length-1){
+      current=0;
+      $("#mason-image").attr("src", image[current].src);
+    }
+    else{
+      ++current;
+      $("#mason-image").attr("src", image[current].src);
+    }
+  });
+
+})
+
+$(document).ready(function(){
+  var image=new Array();
+  var current=0;
+
+  image[0]=new Image();
+  image[0].src="images/detailed_product/productdetails_wine.jpg";
+  image[1]=new Image();
+  image[1].src="images/detailed_product/productdetails_wine_context.jpg";
+  image[2]=new Image();
+  image[2].src="images/detailed_product/productdetails_wine_closeup.jpg";
+
+  
+
+   $("#left2").click(function(){
+    if(current-1<0){
+      current=image.length-1;
+      $("#wine-image").attr("src", image[current].src);
+    }
+    else{
+      --current;
+      $("#wine-image").attr("src", image[current].src);
+    }
+  });
+
+  $("#right2").click(function(){
+    if(current+1>image.length-1){
+      current=0;
+      $("#wine-image").attr("src", image[current].src);
+    }
+    else{
+      ++current;
+      $("#wine-image").attr("src", image[current].src);
+    }
+  });
+
+})
+
+
+
+
+
